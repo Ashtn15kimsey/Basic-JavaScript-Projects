e7274c2a953cd259be9bac4f9210908b9429283a
ashtn15kimsey <ashtn15@gmail.com>
2021-03-01 12:52:04 -0800

    project_2

---
+++ project2_function/JS/main.js
@@ -0,0 +1,5 @@
+function myfunction(){
+    var sentence="The cat is happy while the dog is angry";
+    sentence=+"even the owner noticed this.";
+    document.getElementById("concatenate").innerHTML=sentence;
+}
