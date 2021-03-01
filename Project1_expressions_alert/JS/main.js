895bca30396118c91f3140a9357b39f766ee1b0a
ashtn15kimsey <ashtn15@gmail.com>
2021-03-01 11:15:32 -0800

    Initial commit

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+
+document.write(900000*400); //adding two inputs and putting out a reslut.
+
+var B= "The pizza is hot";
+ window.alert(B);
+
+ var h ="That window is too tinted and is illegal";
+ document.write(h);
