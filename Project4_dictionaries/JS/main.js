c1f88027921f0eeda377c1c675d79a0913012cfd
ashtn15kimsey <ashtn15@gmail.com>
2021-03-01 20:52:11 -0800

    update

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,12 @@
+function my_Dictionary() {
+    var animal= {
+        species:"cat",
+        color:"yellow",
+        breed:"bengal cat",
+        age:10,
+        sounds:"Bark!"
+    };
+    delete animal sound;
+    document.getElementById("Dictionary").innerHTML=Animal.sound;
+    }
+}
