9c426f3b2a4b8f14917f0a46a778b14418ed950a
ashtn15kimsey <ashtn15@gmail.com>
2021-03-02 10:27:37 -0800

    update
    project5

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,45 @@
+document.write(typeof "word");
+document.write(typeof 7);
+
+function my_Function() {
+    document.getElementsByTagName("Test").innerHTML=0/0;
+}
+{document.write(2E3310);}
+
+{document.write(-3E310);}
+
+{document.write(10>2);}
+
+{document.write(20<10);}
+
+console.log(7+10);
+document.write("10"+5);
+
+document.write(8==8);
+document.write(4==8);
+
+x=8;
+y=9;
+document.write(x===y);
+ 
+x=13;
+y="20";
+document.write(x===y);
+
+a="magnus";
+b="magnus";
+document.writes(a===b);
+
+document.write(5>10 && 10>4);
+
+document.write(5>10 || 10<4);
+
+function not_function() {
+    document.getElementsById("not").innerHTML=!(20>10);
+    }
+    function not_function() {
+        document.getElementsById("not").innerHTML=!(5>10);
+        }
+           
+
+
