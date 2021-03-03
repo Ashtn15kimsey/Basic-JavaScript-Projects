d692434418b1e8bd8ce8d07c4620e54d791fe133
ashtn15kimsey <ashtn15@gmail.com>
2021-03-03 11:31:37 -0800

    project9

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -1,18 +1,34 @@
-var slideIndex = 0;
+function plusSlides(n, no) {
+    showSlides(slideIndex[no] += n, no);
+    doucument.showSlides
+  }  
+  
+  var slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
-   showDivs(slideIndex)
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
-function showDivs(n){
-    var i;
-    var x=
-    document.getElementByIdClassNames("myslides");
-     if(n>x.length) {slideIndex=1}
-     if(n<1){slideIndex=x.length};
-     for(i=0;  i<x.length; i++){
-         x[i].style.display="non";
-     }
-     x[slideindex-1].style.display="block";
-     setTimeout(carousel,200);
-}
+
