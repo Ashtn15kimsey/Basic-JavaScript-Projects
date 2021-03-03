ac6cf2b04de4ed9a35a27bf3091e6c5a2cdc7c0e
ashtn15kimsey <ashtn15@gmail.com>
2021-03-03 10:06:06 -0800

    Update main.js

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -1,13 +1,17 @@
-function countdown() {
-    var seconds=document.getElementById("seconds").nodeValue
-    function tick(){
-        seconds=seconds-1;
-        TimeRanges.innerHTML=seconds;
-        setTimeout(tick 100);
-        if(second==-1){
-            alert("times up!");
-        }
-            
-    }     
-    tick();
+f
+}
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides=
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
