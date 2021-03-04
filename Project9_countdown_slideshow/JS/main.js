5e34cac0534295dcf12493870c97d576226acfba
ashtn15kimsey <ashtn15@gmail.com>
2021-03-03 17:46:58 -0800

    Update main.js

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -30,35 +30,18 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  
-  var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
-
-  // Update the count down every 1 second
-  var x = setInterval(function() {
-  
-    // Get today's date and time
-    var now = new Date().getTime();
-  
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-  
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-    // Display the result in the element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-  
-    // If the count down is finished, write some text
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-  }, 1000);
- 
-  
 }
 
+  function countdown() {
+      var seconds=document.getElementsById("seconds").value;
+    function tick(){
+      seconds=seconds-1;
+      timer.innerHTML=seconds;
+      setTimeout(tick, 1000);
+    if(seconds==-1){
+        alert("Time's Up!");
+      }
+          }
+      tick();    
+        }
+    
